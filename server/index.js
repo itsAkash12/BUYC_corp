236eacc09ec779d23a92a1010e8aae9e324d9dd3
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-05-25 11:13:37 +0530

    Implemented: login and signup from backend

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
-const auth = require("./routes/auth.routes");
+const auth = require("./routes/Auth.routes");
 const connection  = require("./config/db");
 const app = express();
 app.use(cors());
